ea7a3022dc73760c8a3fafbb2bc166b615cea327
aanklewicz <aanklewicz@NCMBCK.local>
2014-12-14 11:41:17 -0500

    first rev of project

---
+++ Postcard/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Postcard
+//
+//  Created by Adam M. Anklewicz on 2014-12-13.
+//  Copyright (c) 2014 Never Had To Fight. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    @IBOutlet weak var nickItButton: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func nickItButtonPressed(sender: UIButton) {
+        // Unhides messageLabel, sets it to the value of enterMessageTextField, and makes it red.
+        messageLabel.hidden = false
+        messageLabel.text = enterMessageTextField.text
+        messageLabel.textColor = UIColor.redColor()
+        
+        // Clears out the text to blank, and hides the keyboard.
+        enterMessageTextField.text = ""
+        enterMessageTextField.resignFirstResponder()
+        
+        // Here we set the button text to "Mail Sent" rather than "Nick It!"
+        nickItButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+    }
+
+}
+
